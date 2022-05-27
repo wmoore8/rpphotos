bf5484e1f6482391ee4a1db72320657455b62c88
Ramsey's Desk <maison@Ramseyss-Mini.attlocal.net>
2022-05-27 16:06:43 -0500

    centering content

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{fontSize: 30}}>
+    <div className="App" style={{fontSize: 30, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
       (TEMP)
     </div>
   );
