66f8cb6f78521f978903b019f917fc05f5fc8461
Willy Moore <willymoore@willys-mbp.lan>
2022-05-28 11:21:38 -0500

    add temp images, begin carousel background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import desertImg from './assets/desert.jpg'
+import mountainImg from './assets/mountain.jpg'
+import rocksImg from './assets/rocks.jpg'
+import lightsImg from './assets/lights.jpg'
+
 function App() {
-  return (
-    <div className="App">
-        testing again from home
-    </div>
-  );
+
+    const backgroundImageList = [
+        desertImg,
+        mountainImg,
+        rocksImg,
+        lightsImg,
+    ]
+
+    const [backgroundImage, setBackgroundImage] = useState(backgroundImageList[0])
+
+    useEffect(() => {
+        setInterval(() => setBackgroundImage(backgroundImageList[1]), 5000)
+    }, [])
+
+    return (
+        <div className="App">
+            <img
+              alt='carousel background'
+              src={backgroundImage}
+              style={{
+                height: '100vh',
+                width: '100vw'
+              }}
+          />
+
+        </div>
+    );
 }
 
 export default App;
