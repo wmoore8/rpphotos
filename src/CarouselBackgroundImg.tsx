4207712f1686defc10da4d0ca4532a77a34e8b63
Ramsey's Desk <maison@Ramseyss-Mini.attlocal.net>
2022-06-01 15:39:52 -0500

    add 50 carousel background images

--- src/CarouselBackgroundImg.tsx
+++ src/CarouselBackgroundImg.tsx
@@ -1,20 +1,18 @@
 import React, {useEffect, useState} from 'react'
 
-import desertImg from './assets/desert.jpg'
-import mountainImg from './assets/mountain.jpg'
-import rocksImg from './assets/rocks.jpg'
-import lightsImg from './assets/lights.jpg'
+import {CB1, CB2, CB3, CB4, CB5, CB6, CB7, CB8, CB9, CB10, CB11, CB12, CB13, CB14, CB15, CB16, CB17, CB18, CB19, CB20,
+    CB21, CB22, CB23, CB24, CB25, CB26, CB27, CB28, CB29, CB30, CB31, CB32, CB33, CB34, CB35, CB36, CB37, CB38, CB39,
+    CB40, CB41, CB42, CB43, CB44, CB45, CB46, CB47, CB48, CB49, CB50, wingedMan} from './assets/index'
 
 export default function CarouselBackgroundImg () {
 
-    const [backgroundImage, setBackgroundImage] = useState(desertImg)
+    const [backgroundImage, setBackgroundImage] = useState(wingedMan)
 
     useEffect(() => {
         const backgroundImageList = [
-            desertImg,
-            mountainImg,
-            rocksImg,
-            lightsImg,
+            CB1, CB2, CB3, CB4, CB5, CB6, CB7, CB8, CB9, CB10, CB11, CB12, CB13, CB14, CB15, CB16, CB17, CB18, CB19,
+            CB20, CB21, CB22, CB23, CB24, CB25, CB26, CB27, CB28, CB29, CB30, CB31, CB32, CB33, CB34, CB35, CB36, CB37,
+            CB38, CB39, CB40, CB41, CB42, CB43, CB44, CB45, CB46, CB47, CB48, CB49, CB50, wingedMan
         ]
 
         let count = 0
@@ -22,7 +20,7 @@ export default function CarouselBackgroundImg () {
         setInterval(() => {
             count === backgroundImageList.length - 1 ? count = 0 : count = count + 1
             setBackgroundImage(backgroundImageList[count])
-        }, 5000)
+        }, 2000)
     }, [])
 
     return (
---
+++ src/assets/index.ts
@@ -0,0 +1,105 @@
+import CB1 from './3-10-19_RSH_BJ_1C.jpg'
+import CB2 from './3-10-19_RSH_BJ_1D.jpg'
+import CB3 from './flowerScene.JPG'
+import CB4 from './RSH_Brandon_1C.jpg'
+import CB5 from './RSH_Brandon_2.jpg'
+import CB6 from './RSH_Brandon_3C.jpg'
+import CB7 from './RSH_Jake_3C.jpg'
+import CB8 from './RSH_Jake_4C.jpg'
+import CB9 from './RSH_James_1B2.jpg'
+import CB10 from './RSH_James_2.jpg'
+import CB11 from './RSH_James_4C.jpg'
+import CB12 from './RSH_James_6.jpg'
+import CB13 from './RSH_James_10.jpg'
+import CB14 from './RSH_James_11B.jpg'
+import CB15 from './RSH_JF_1.jpg'
+import CB16 from './RSH_JF_2.jpg'
+import CB17 from './RSH_JF_11D.jpg'
+import CB18 from './RSH_Joey_1.jpg'
+import CB19 from './RSH_Joey_3C.jpg'
+import CB20 from './RSH_LA_1.jpg'
+import CB21 from './RSH_LA_7C.jpg'
+import CB22 from './RSH_RJP_2L.jpg'
+import CB23 from './RSH_RJP_3.jpg'
+import CB24 from './RSH_RJP_4-5-22_1.jpg'
+import CB25 from './RSH_RJP_6.jpg'
+import CB26 from './RSH_RJP_8.jpg'
+import CB27 from './RSH_RJP_8S.jpg'
+import CB28 from './RSH_RJP_11S.jpg'
+import CB29 from './RSH_RJP_16.jpg'
+import CB30 from './RSH_RJP_20BIG-Nude.jpg'
+import CB31 from './RSH_RJP_21C.jpg'
+import CB32 from './RSH_RJP_23C.jpg'
+import CB33 from './RSH_RJP_25D.jpg'
+import CB34 from './RSH_RJP_27A.jpg'
+import CB35 from './RSH_RJP_27C.jpg'
+import CB36 from './RSH_RJP_28.jpg'
+import CB37 from './RSH_RJP_29.jpg'
+import CB38 from './RSH_RJP_29A.jpg'
+import CB39 from './RSH_RJP_30C.jpg'
+import CB40 from './RSH_RJP_33D.jpg'
+import CB41 from './RSH_RJP_C_4.jpg'
+import CB42 from './RSH_RJP_C_5.jpg'
+import CB43 from './RSH_RJP_C_39.jpg'
+import CB44 from './RSH_RJP_D_1C.jpg'
+import CB45 from './RSH_RJP_D_4B.jpg'
+import CB46 from './RSH_RJP_D_5F.jpg'
+import CB47 from './RSH_RJP_D_9.jpg'
+import CB48 from './RSH_RJP_T_2C.jpg'
+import CB49 from './RSH_RJP_T_10.jpg'
+import CB50 from './RSH_RJP_T_11.jpg'
+import wingedMan from './wingedMan.jpg'
+
+export {
+    CB1,
+    CB2,
+    CB3,
+    CB4,
+    CB5,
+    CB6,
+    CB7,
+    CB8,
+    CB9,
+    CB10,
+    CB11,
+    CB12,
+    CB13,
+    CB14,
+    CB15,
+    CB16,
+    CB17,
+    CB18,
+    CB19,
+    CB20,
+    CB21,
+    CB22,
+    CB23,
+    CB24,
+    CB25,
+    CB26,
+    CB27,
+    CB28,
+    CB29,
+    CB30,
+    CB31,
+    CB32,
+    CB33,
+    CB34,
+    CB35,
+    CB36,
+    CB37,
+    CB38,
+    CB39,
+    CB40,
+    CB41,
+    CB42,
+    CB43,
+    CB44,
+    CB45,
+    CB46,
+    CB47,
+    CB48,
+    CB49,
+    CB50,
+    wingedMan,
+}
