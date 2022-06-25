84f7a9efd3c434e763a574ba77a95930231d325d
Ramsey's Desk <maison@Ramseyss-Mini.attlocal.net>
2022-06-25 11:57:55 -0500

    delete vertical carousels

--- src/CarouselBackgroundImg.tsx
+++ src/CarouselBackgroundImg.tsx
@@ -1,19 +1,52 @@
 import React from 'react'
-import { Fade } from 'react-slideshow-image';
+import {Fade} from 'react-slideshow-image';
 
 import 'react-slideshow-image/dist/styles.css'
 import './stylesheets/CarouselBackgroundImg.css'
 
-import {CB1, CB2, CB4, CB5, CB6, CB7, CB8, CB9, CB10, CB11, CB12, CB13, CB14, CB15, CB16, CB17, CB18, CB19, CB20,
-    CB21, CB22, CB23, CB24, CB25, CB26, CB27, CB28, CB29, CB30, CB31, CB32, CB33, CB34, CB35, CB36, CB37, CB38, CB39,
-    CB40, CB41, CB42, CB43, CB44, CB45, CB46, CB47, CB48, CB49, CB50, wingedMan} from './assets/index'
+import {
+    CB1,
+    CB10,
+    CB11,
+    CB12,
+    CB13,
+    CB14,
+    CB15,
+    CB16,
+    CB17,
+    CB18,
+    CB2,
+    CB22,
+    CB23,
+    CB24,
+    CB25,
+    CB26,
+    CB27,
+    CB29,
+    CB30,
+    CB32,
+    CB35,
+    CB40,
+    CB42,
+    CB43,
+    CB44,
+    CB46,
+    CB47,
+    CB48,
+    CB49,
+    CB50,
+    CB7,
+    CB8,
+    CB9,
+    wingedMan
+} from './assets/index'
 
 export default function CarouselBackgroundImg () {
 
     const backgroundImageList = [
-        CB1, CB2, CB4, CB5, CB6, CB7, CB8, CB9, CB10, CB11, CB12, CB13, CB14, CB15, CB16, CB17, CB18, CB19,
-        CB20, CB21, CB22, CB23, CB24, CB25, CB26, CB27, CB28, CB29, CB30, CB31, CB32, CB33, CB34, CB35, CB36, CB37,
-        CB38, CB39, CB40, CB41, CB42, CB43, CB44, CB45, CB46, CB47, CB48, CB49, CB50, wingedMan
+        CB1, CB2, CB7, CB8, CB9, CB10, CB11, CB12, CB13, CB14, CB15, CB16, CB17, CB18,
+        CB22, CB23, CB24, CB25, CB26, CB27, CB29, CB30, CB32, CB35,
+        CB40, CB42, CB43, CB44, CB46, CB47, CB48, CB49, CB50, wingedMan
     ]
 
     return (
