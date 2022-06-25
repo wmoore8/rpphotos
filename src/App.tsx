d283d757f4b5374802fffc90b0e511d9c4cc4910
Ramsey's Desk <maison@Ramseyss-Mini.attlocal.net>
2022-06-25 11:36:59 -0500

    testing circlci + centered content

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import CarouselBackgroundImg from "./CarouselBackgroundImg";
 function App() {
 
     return (
-        <div className='app'>
+        <>
             <CarouselBackgroundImg/>
+            <div className='app'>
 
-            <div className='links-container'>
-                <span className='links'>RAMSEY PRINCE</span>
-                <span className='links'>About</span>
-                <span className='links'>Over 18</span>
+                <div className='links-container'>
+                    <span className='links'>RAMSEY PRINCE</span>
+                    <span className='links'>About</span>
+                    <span className='links'>Over 18</span>
+                </div>
+
             </div>
-
-        </div>
+        </>
     );
 }
 
