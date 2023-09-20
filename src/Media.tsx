c7bdd37e06a0249e1ffe5616fe680cba180c50ee
Willy Moore <willymoore@willys-mbp.lan>
2023-09-20 13:25:35 -0500

    fall update

--- src/Media.tsx
+++ src/Media.tsx
@@ -12,10 +12,10 @@ export default function Media() {
 
             <RPFont variant='h4' padding='20px 0'> Médias </RPFont>
 
-            <RPFont> The Baton Show Lounge - 6/15 </RPFont>
+            <RPFont> The Baton Show Lounge - 9/21 </RPFont>
 
             <a
-                href='https://www.eventbrite.com/e/burlesque-royale-ramseys-birthday-pride-celebration-tickets-644440357457'
+                href='https://www.eventbrite.com/e/burlesque-royale-september-tickets-710022555577?aff=oddtdtcreator'
                 style={{
                     textDecoration: 'none',
                     color: 'white',
@@ -23,6 +23,21 @@ export default function Media() {
                     fontFamily: 'metropolis',
                     padding: '10px 0'
             }}
+            >
+                Tickets Here
+            </a>
+
+            <RPFont> ANNIVERSARY CELEBRATION SHOW - 11/16 </RPFont>
+
+            <a
+                href='https://www.eventbrite.com/e/1-year-anniversary-celebration-tickets-710032806237?aff=oddtdtcreator'
+                style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    fontSize: '20px',
+                    fontFamily: 'metropolis',
+                    padding: '10px 0'
+                }}
             >
                 Tickets Here
             </a>
